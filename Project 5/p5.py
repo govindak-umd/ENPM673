a88b56ec30fdfe8f686497823ee174e216b586f4
rajesh1996 <rns@umd.edu>
2020-04-30 15:46:03 -0400

    Camera pose estimation

--- Project 5/p5.py
+++ Project 5/p5.py
@@ -271,9 +271,39 @@ print('E : : : > > > ', E)
 
 #DECOMPOSING THE ESSENTIAL MATRIX TO TRANSLATION AND ROTATION MATRIX
 
-U_decompose, S_decompose, V_decompose = np.linalg.svd(E)
-W = np.array([0.0, -1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0]).reshape(3, 3)
 
+def cameraposestimation(E):
+    
+    U_decompose, S_decompose, V_decompose = np.linalg.svd(E)
+    W = np.array([0.0, -1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0]).reshape(3, 3)
+    
+    R1 = np.matmul(np.matmul(U_decompose,W),V_decompose)
+    R2 = np.matmul(np.matmul(U_decompose,W),V_decompose)
+    R3 = np.matmul(np.matmul(U_decompose,W.T),V_decompose)
+    R4 =  np.matmul(np.matmul(U_decompose,W.T),V_decompose)
+    
+    C1 = U_decompose[:,2]
+    C2 = -U_decompose[:,2]
+    C3 = U_decompose[:,2]
+    C4 = -U_decompose[:,2]
+    
+    if (np.linalg.det(R1)<0):
+        R1 = -R1
+        C1 = -C1
+    if (np.linalg.det(R2)<0):
+        R2 = -R2
+        C2 = -C2    
+    if (np.linalg.det(R3)<0):
+        R3 = -R3
+        C3 = -C3    
+    if (np.linalg.det(R4)<0):
+        R4 = -R4
+        C4 = -C4
+        
+    return [R1,R2,R3,R4],[C1,C2,C3,C4]
+
+
+R,T = cameraposestimation(E)
 # new_list = np.array([features_1[5][0] ,features_2[5][1] ,1])
 # new_list = np.reshape(new_list,(3,1))
 # print(new_list)
